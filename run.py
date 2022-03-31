e5985a4237e24ffe70b1a8a501a14e6ec32f3dc0
Marc Leerink <marc.leerink@code.berlin>
2022-03-31 14:40:57 +0200

    try new heroku server

--- run.py
+++ run.py
@@ -1,14 +1,6 @@
-# from app.app import create_app
+from app.app import create_app
 
-# app = create_app()
+app = create_app()
 
-# if __name__ == '__main__':
-#     app.run()
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "hello"
+if __name__ == '__main__':
+    app.run()
