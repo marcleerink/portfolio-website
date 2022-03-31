22ae2d8d7b3664f372f86e6d38a00c8512d06f3f
Marc Leerink <marc.leerink@code.berlin>
2022-03-31 16:03:14 +0200

    heroku databse config

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
-from dotenv import load_dotenv
 from os import environ
+from dotenv import load_dotenv
 
 load_dotenv()
+
 SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL').replace('postgres://', 'postgresql://', 1)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False
