28126b96107365ea72a25b7b786ad19fd70474b9
Marc Leerink <marc.leerink@code.berlin>
2022-03-31 14:53:25 +0200

    pocfile and run.py update

--- app/Procfile
+++ app/Procfile
@@ -1,2 +1,1 @@
-web: gunicorn run:run
-
+web: gunicorn run:app
--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app.app import create_app
 
-app = create_app()
+def app():
+    app = create_app()
+    return app
 
 if __name__ == '__main__':
     app.run()
