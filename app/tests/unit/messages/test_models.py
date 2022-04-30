216a56f094aa75593289deb778f367bdc06978d0
Marc Leerink <marc.leerink@code.berlin>
2022-04-30 12:19:16 +0200

    update gitignore

--- app/tests/unit/messages/test_models.py
+++ app/tests/unit/messages/test_models.py
@@ -1,16 +1,27 @@
+from pickletools import uint4
 import pytest
 from app.messages.models import Message, User
 from app.extensions.database import db
 
 
-def test_user_update(client):
-    # updates User model
-    user = User(name = 'Test tester', email = 'test@test.com', password = 'testtest')
+def test_user_delete(client):
+    # deletes record User model
+    user = User(name = 'Test tester', email = 'test20@test.com', password = 'testtest')
     db.session.add(user)
     db.session.commit()
 
-    user.email = 'maleerink@gmail.com'
-    user.save()
+    user.delete()
 
-    updated_user = User.query.filter_by(name='Test tester').first()
-    assert updated_user.email == 'maleerink@gmail.com'
+    deleted_user = User.query.filter_by(name='Test tester').first()
+    assert deleted_user is None
+
+def test_message_delete(client):
+    # deletes record Message model
+    message = message(subject = 'Test tester', message = 'sgsdgsdgsdgsdgdggdsc')
+    db.session.add(message)
+    db.session.commit()
+
+    message.delete()
+
+    deleted_message = User.query.filter_by(subject='Test tester').first()
+    assert deleted_message is None
--- app/tests/unit/messages/test_routes.py
+++ app/tests/unit/messages/test_routes.py
@@ -1,7 +1,17 @@
 from app.messages.models import Message
 
+def test_get_login_renders(client):
+    # Page loads and renders login
+    response = client.get('/log-in')
+    assert b'Log In' in response.data
+
+def test_get_signup_renders(client):
+    # Page loads and renders signup
+    response = client.get('/sign-up')
+    assert b'Sign Up' in response.data
+
 def test_get_contact_renders(client):
-    # Page loads and renders contact
+    # Page loads and renders signup
     response = client.get('/contact')
     assert b'Contact' in response.data
 
@@ -11,4 +21,4 @@ def test_post_contact_sends_message(client):
         'subject' : 'Test',
         'message' : 'testtest',
     })
-    assert Message.query.first() is None
+    assert Message.query.first() is not None 
